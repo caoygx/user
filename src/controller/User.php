e81b760681be9b85fdbe1ed88885eb8d7f549df7
junfei <junfei@qq.com>
2019-09-20 11:41:29 +0800

    i

---
+++ src/controller/User.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\controller;
+
+
+
+class User
+{
+
+    /**
+     * @param  string $name 数据名称
+     * @return mixed
+     * @Route("register/:name")
+     */
+    function index(){
+        return view();
+    }
+}
